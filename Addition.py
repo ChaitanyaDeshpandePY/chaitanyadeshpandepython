eb93245b50895b9a93dcde56fc5a081b99b6d883
ChaitanyaDeshpandePY <42547696+ChaitanyaDeshpandePY@users.noreply.github.com>
2018-09-03 19:24:57 +0530

    Add files via upload

---
+++ Addition.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/python3.6
+
+#Write a Program to accept the 2 numbers from the user and display their addition
+#Developed by : Chaitanya Deshpande
+#Date : 28-08-2018
+#Script Name : Addition.py
+
+
+#Program Begins
+
+# Accepting User Inputs
+num1 = input("Enter First number : ")
+num2 = input("Enter Second number : ")
+int(num1)
+int(num2)
+
+# Addition of 2 Numbers
+num3 = int(num1) + int(num2)
+
+# Printing the Addition of 2 Numbers
+print("Addition of 2 numbers is : ")
+print (int(num3))
+
+# Program Ends
+
+ 
