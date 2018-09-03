0ff3fe475f3dbf21b593c5f5e17f4034a1793637
ChaitanyaDeshpandePY <42547696+ChaitanyaDeshpandePY@users.noreply.github.com>
2018-09-03 19:36:26 +0530

    Commit
    Trial

---
+++ Hello.py
@@ -0,0 +1,19 @@
+#! /usr/local/bin/python3.6
+
+# Write a Python Program to Accept a name from user and greet him with the message “Hello <name>”
+# Developed by : Chaitanya Deshpande
+# Date : 28-08-2018
+# Script Name : Hello.py
+
+ 
+
+# Program Begins
+
+# Accepting the User Input
+name = input("Enter User Input : ")
+str(name)
+
+# Printing / Greeting with Input
+print("Hello" + " " + str(name))
+
+# Program Ends
